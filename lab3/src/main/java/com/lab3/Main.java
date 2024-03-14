5b6416f7b265b0c53bb2f0c8c2e758413a88e676
jerzyszyjut <jerzy.szyjut@outlook.com>
2024-03-14 20:46:44 +0100

    Lab 3 hello world

---
+++ lab3/src/main/java/com/lab3/Main.java
@@ -0,0 +1,13 @@
+package com.lab3;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
